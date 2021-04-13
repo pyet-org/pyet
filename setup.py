818a8a780b97680e58831156532265a379bf514e
MatevzVremec <matevz.vremec@gmail.com>
2021-04-13 14:52:39 +0200

    Updated docs

--- setup.py
+++ setup.py
@@ -5,10 +5,26 @@ setup(
     version='0.2b',
     packages=[''],
     url='https://github.com/phydrus/pyet',
-    license='GNU General public version 3.0',
+    license='MIT License',
     author='Matevz Vremec, Raoul Collenteur',
     author_email='matevz.vremec@uni-graz.at, raoul.collenteur@uni-graz.at',
     description='pyet',
     test_suite='tests',
-    install_requires=['matplotlib>=3.0', 'pandas>=1.0'],
+    project_urls={
+            'Source': 'https://github.com/phydrus/pyet',
+            'Tracker': 'https://github.com/phydrus/pyet/issues',
+            'Help': 'https://github.com/phydrus/pyet/discussions'
+    },
+    classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Science/Research',
+            'Intended Audience :: Other Audience',
+            'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'Topic :: Scientific/Engineering :: Hydrology',
+    ],
+    install_requires=['numpy>=1.15', 'matplotlib>=2.0', 'pandas>=1.0',
+                      'scipy>=1.1'],
 )
