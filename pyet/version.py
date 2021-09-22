6e04f1503867c2a4183478c2965b8bd546e563a2
MatevzVremec <matevz.vremec@gmail.com>
2021-09-22 14:14:54 +0200

    Update version.py

--- pyet/version.py
+++ pyet/version.py
@@ -1,3 +1,3 @@
 # This is the only location where the version will be written and changed.
 # Based on https://packaging.python.org/single_source_version/
-__version__ = "1.1.0b"
+__version__ = "1.0.1b"
