470f49f13301991f8fb082703b0045922db3a4fa
Raoul Collenteur <raoulcollenteur@gmail.com>
2022-11-07 10:05:33 +0100

    update show_versions method

--- pyet/utils.py
+++ pyet/utils.py
@@ -9,24 +9,19 @@ def show_versions():
     from pyet import __version__ as ps_version
     from pandas import __version__ as pd_version
     from numpy import __version__ as np_version
-    from scipy import __version__ as sc_version
-    from matplotlib import __version__ as mpl_version
     from sys import version as os_version
 
     msg = (
         f"Python version: {os_version}\n"
         f"Numpy version: {np_version}\n"
-        f"Scipy version: {sc_version}\n"
         f"Pandas version: {pd_version}\n"
-        f"Matplotlib version: {mpl_version}\n"
         f"Pyet version: {ps_version}"
     )
     return print(msg)
 
 
 def check_lat(lat):
-    """Method to check if latitude was given in radians and transforms to
-    radians, if latitude given in degrees. """
+    """Method to check if latitude was (most likely) given in radians."""
     if -1.6 < numpy.mean(lat) < 1.6:
         return lat
     else:
